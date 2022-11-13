73587f03a6842e87441b983403e9ecdfd81f0733
Gino-Y <ginoyan250@gmail.com>
2022-11-13 19:14:10 +0800

    Merge remote-tracking branch 'origin/main'

--- backProject/test/chapter05.py
+++ backProject/test/chapter05.py
@@ -6,11 +6,36 @@ app05 = APIRouter()
 """Dependencies 创建、导入和声明依赖"""
 
 
-async def common_parameters(q: Optional[str] = None, page: int = 1, limit: int = 100):
+async def common_parameters(q: Optional[str] = None,
+                            page: int = 1,
+                            limit: int = 100):
     return {'q': q, 'page': page, 'limit': limit}
 
 
 @app05.get('/dependency')
-async def dependency01(commons: dict = Depends(common_parameters)):
+async def dependency(commons: dict = Depends(common_parameters)):
     return commons
 
+
+"""Classes as Dependencies 类作为依赖项"""
+
+fake_items_db = [{'item_name': 'Foo'}, {'item_name': 'Bar'}, {'item_name ': 'Baz'}]
+
+
+class CommonQueryParams:
+    def __init__(self, q: Optional[str] = None,
+                 page: int = 0,
+                 limit: int = 100):
+        self.q = q
+        self.page = page
+        self.limit = limit
+
+
+@app05.get('/classes_as_dependencies')
+async def classes_as_dependencies(commons=Depends(CommonQueryParams)):
+    response = {}
+    if commons.q:
+        response.update({'q': commons.q})
+    items = fake_items_db[commons.page: commons.page + commons.limit]
+    response.update({'items': items})
+    return response
