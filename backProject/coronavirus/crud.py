e5e753d3529ccc7db5024cc75a04aac6cd1f4daf
Gino-Y <ginoyan250@gmail.com>
2022-11-19 22:36:11 +0800

    Merge remote-tracking branch 'origin/main'

--- backProject/coronavirus/crud.py
+++ backProject/coronavirus/crud.py
@@ -1,2 +1,26 @@
 from sqlalchemy.orm import Session
 
+from coronavirus import models, schemas
+
+
+async def get_city(db: Session, city_id: int):
+    return db.query(models.City).filter(models.City.id == city_id).first()
+
+
+async def get_city_by_name(db: Session, name: str):
+    return db.query(models.City).filter(models.City.province == name).first()
+
+
+def get_cities(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.City).offset(skip).limit(limit).all()
+
+
+def create_city(db: Session, city: schemas.CreateCity):
+    db_city = models.City(**city.dict())
+    db.add(db_city)
+    db.commit()
+    db.refresh(db_city)
+    return db_city
+
+
+
